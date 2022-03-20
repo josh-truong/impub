4541f7b998c7a644a1342f9272deb808969bcc96
Joshua Truong <joshktruong@gmail.com>
2022-03-20 13:06:53 -0600

    Fixed Latex bug

--- src/components/Latex/Latex.js
+++ src/components/Latex/Latex.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Latex = (props) => {
+  let node = React.createRef();
+  useEffect(() => {
+    renderMath();
+  });
+
+  const renderMath = () => {
+    window.MathJax.Hub.Queue([
+      "Typeset",
+      window.MathJax.Hub,
+      node.current
+    ]);
+    window.MathJax.Hub.Config({
+      tex2jax: {
+        inlineMath: [['$','$'], ['\\(','\\)']]
+      }
+    });
+  }
   return (
     <>
       {props.expr}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
-import RenderLatex from './components/Latex/RenderLatex';
 
 ReactDOM.render(
   <React.StrictMode>
-      <RenderLatex>
-      <App />
-    </RenderLatex>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
