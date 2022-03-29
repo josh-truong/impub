fbcbb8880adbef2d73aa27d128a89b1f8d5c9637
Joshua Truong <joshktruong@gmail.com>
2022-03-29 14:58:37 -0600

    Integrate mathjs and latex support

--- src/components/Calculator/js/Scope.js
+++ src/components/Calculator/js/Scope.js
@@ -1,21 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
 
+import Slider from './Slider'
+import Latex from '../../Latex/Latex'
+var math = require('mathjs')
 
 /*
-  https://stackoverflow.com/questions/30474506/replace-part-of-string-with-tag-in-jsx
-  Think about using math js
-  - Sigma Sum?? anonymous functions
-  - Latex -> Calculator component?
-    - How should we process this
-    - Is it possible to create anonymous functions from latex format?
-      - Can we avoid writing the same function twice?
+  Reference:
+  - https://mathjs.org/docs/expressions/parsing.html
 */
 const Scope = () => {
+  const [error, setError] = useState(false)
+  const [scope, setScope] = useState({ k:0, n:0 }) // Variable scope
+  const [mathJsExpr, setMathJsExpr] = useState('k*n+1') // math js expression to evaluate
+  const [latexExpr, setLatexExpr] = useState(math.parse(mathJsExpr).toTex()) // Convert math js expression to tex
+  const [result, setResult] = useState(0) // Store evaluated expression
+
+
+
+  function handleCallBack(e) {
+    const key = Object.keys(e)
+    setScope(prevState => ({
+      ...prevState,
+      [key]: e[key] // Update variable value
+    }))
+
+    
+    let expr;
+    try { expr = math.parse(mathJsExpr).compile() } // Compile expression
+    catch(error) { setError(true); console.log(error) }
+
+
+    try { // Evaluate given expression
+      const output = expr.evaluate(scope) // Evaluate expression
+      setResult(output)
+    } catch(error) { setError(true); console.log(error) }
+  }
+  
   return (
     <div>
-      
+      {
+        Object.keys(scope).map((key) => (
+          <Slider key={key} id={key} value={handleCallBack} max={10} />
+        ))
+      }
+      <Latex expr={`$${latexExpr} = $`} /> {error ? <span style={{color:'red'}}>{result}</span> : result }
     </div>
   )
 }
 
 export default Scope
+
+
+
+/*
+  <Scope>
+    - Input
+      - Array of variables
+      - Expression to evaluate
+
+    - Process
+      - How to refer a variable within scope
+        - Can global variables work?
+
+  - If not all variables are present and math.evaluate returns compile error display only latex and change stated variable to number
+
+
+
+
+*/
