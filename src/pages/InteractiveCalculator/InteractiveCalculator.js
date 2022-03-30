fea8616e4e458d41d009ae8c0d8a36e7a20d1432
Joshua Truong <joshktruong@gmail.com>
2022-03-30 02:18:20 -0600

    pseudocode

--- src/pages/InteractiveCalculator/InteractiveCalculator.js
+++ src/pages/InteractiveCalculator/InteractiveCalculator.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import { Scope } from "../../components/Calculator/Calculator"
-
+import React, {useState} from 'react'
+import { Var, Evaluate, Scope } from "../../components/Calculator/Calculator"
 const InteractiveCalculator = () => {
+  const [val, setVal] = useState({});
+  
+
   return (
     <div>
-      <Scope>
-
-          
-      </Scope>
+      <Scope />
+      
+      {/* <Var id={{'eq':'k'}} min={1} max={10} />
+      <Var id={{'eq':'n'}} min={1} max={10} />
+      <Evaluate id='eq' expr='k*n+1'/> */}
     </div>
   )
 }
 
 export default InteractiveCalculator
+
+
+/*
+  Scope is to declare expression and variables
+  - Will be added to GlobalVars
+
+  Var will render slider and input box for interactivity
+  - Var should be declared before Evaluate
+  
+  Evaluate will evaluate expression
+  - If not all variables are well-defined substitue only those variables
+  - If all variables are well-defined, evaluate expreesion
+
+  Add a feature where if mathjs expression is valid then render expreesion
+  in tex form
+*/
