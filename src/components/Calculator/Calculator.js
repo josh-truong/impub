867ef3f9434c854e4328c2cfb08e6d4c075ab906
Joshua Truong <joshktruong@gmail.com>
2022-03-30 02:16:47 -0600

    New components

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,2 +1,6 @@
 export {default as Scope} from "./js/Scope";
 export {default as Slider} from "./js/Slider";
+
+
+export {default as Var} from "./js/Var";
+export {default as Evaluate} from "./js/Evaluate";
