c09bc95395c9113f5b1d91af11f7e20de701a2ea
Joshua Truong <joshktruong@gmail.com>
2022-04-02 13:03:05 -0600

    redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+// React Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Import Main Application
 import App from './pages/App/App';
+import './index.css';
+
+// Redux Imports
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+import rootReducer from './reducers'
+const store = createStore(
+  rootReducer, /* preloadedState */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() /* Redux devtools extension for browser */
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,15 +1,26 @@
-// import DisplayProof from '../DisplayProof/DisplayProof';
-// import DisplayDefRef from '../DisplayDefRef/DisplayDefRef';
-// import Features from '../Features/Features';
-import InteractiveCalculator from "../InteractiveCalculator/InteractiveCalculator";
+import { DisplayProof, DisplayDefRef, Features, InteractiveCalculator }
+  from '../index.js';
+import  { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../../actions/index'
+
+
 function App() {
+  const counter = useSelector(state => state.counterReducer)
+  const isLogged = useSelector(state => state.loggedReducer)
+  const dispatch = useDispatch();
   return (
     <div className="container">
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? <h3>Oops</h3> : ''}
+
+
       {/* <DisplayProof/> */}
       {/* <DisplayDefRef/> */}
       {/* <Features/> */}
-     
-      <InteractiveCalculator/>
+      {/* <InteractiveCalculator/> */}
     </div>
   );
 }
