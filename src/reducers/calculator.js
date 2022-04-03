679f52435df664fbe8c6fdcd5b4cf4499af523f4
Joshua Truong <joshktruong@gmail.com>
2022-04-02 18:16:09 -0600

    Redux Store will evaluate expression and store it

--- src/reducers/calculator.js
+++ src/reducers/calculator.js
@@ -1,22 +1,33 @@
-import { ADD_SCOPE, UPDATE_VARIABLE } from "../constants"
+import { re } from "mathjs"
+import { ADD_SCOPE, EVAL_EXPR, UPDATE_VARIABLE } from "../constants"
 var math = require('mathjs')
 
-function recursive_items(dict, variables = {}) { // tail recursion method
-    for (const [key, value] of Object.entries(dict)) {
-        if (key === 'args' && dict[key] instanceof Array) {
-            dict[key].forEach(elt => {
-                if (elt.hasOwnProperty('name')) {
-                    variables[Object.values(elt)[0]] = null
-                }
-                else if (elt.hasOwnProperty('content')) {
-                    recursive_items(elt.content, variables)
-                } else {
-                    recursive_items(elt, variables)
-                }
-            })
-        }
-    }
-    return variables
+// function getVariables(dict, variables = {}) { // tail recursion method
+//     for (const [key, value] of Object.entries(dict)) {
+//         if (key === 'args' && dict[key] instanceof Array) {
+//             dict[key].forEach(elt => {
+//                 if (elt.hasOwnProperty('name')) {
+//                     variables[Object.values(elt)[0]] = null
+//                 }
+//                 else if (elt.hasOwnProperty('content')) {
+//                     getVariables(elt.content, variables)
+//                 } else {
+//                     getVariables(elt, variables)
+//                 }
+//             })
+//         }
+//     }
+//     return variables
+// }
+
+function getVariables(node) {
+    const filtered = node.filter(function (node) {
+        return (node.isSymbolNode && node.name)
+    })
+    const distinct = Array.from(new Set(filtered.map((elt) => elt.name)))
+    var var_dict = {}
+    distinct.map((elt) =>  var_dict[elt] = null)
+    return var_dict
 }
 
 const calcReducer = (state={}, action) => {
@@ -26,7 +37,7 @@ const calcReducer = (state={}, action) => {
             const latexExpr = math.parse(mathJsExpr).toTex() // Convert math js expression to tex
         
             const parser = math.parse(mathJsExpr) // Retrieve Abstract Syntax Tree (AST)
-            const variables = recursive_items(parser) // Retrieve variables
+            const variables = getVariables(parser) // Retrieve variables
 
             return {
                 ...state,
@@ -38,17 +49,20 @@ const calcReducer = (state={}, action) => {
                 }
             };
         case UPDATE_VARIABLE:
+            // Check if all required props has been declared
             let requiredProps = action.hasOwnProperty('scope') &&
                                 action.hasOwnProperty('var') &&
                                 action.hasOwnProperty('val');
             if (!requiredProps) { throw 'Props requires scope, var, and val.' }
-            
+
+            // Validate if the declared variable is within scope
             let status = false;
             try {
                 status = !(state[action.scope].var.hasOwnProperty(action.var))
             } catch (error) { throw `Scope ${action.scope} has not been declared!` }
             if (status) { throw `Variable ${action.var} does not exists within scope ${action.scope}!` }
-            return {
+
+            return calcReducer({
                 ...state,
                 [action.scope]: {
                     ...state[action.scope],
@@ -57,6 +71,24 @@ const calcReducer = (state={}, action) => {
                         [action.var]: action.val
                     }
                 }
+            }, { type: EVAL_EXPR, scope: action.scope})
+
+        case EVAL_EXPR:
+            try {
+                const code = math.parse(state[action.scope].mathJsExpr).compile();
+                const scope = state[action.scope].var;
+                return {
+                    ...state,
+                    [action.scope]: {
+                        ...state[action.scope],
+                        result: code.evaluate(scope)
+                    }
+                }
+            }
+            catch(error) {
+                // Return latex format as a result
+                    // if there are unintialized variables
+                return state
             }
         default:
             return state
