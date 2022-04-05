6b64ec2ce34366d34c42f749e16604fc3e8fa112
Joshua Truong <joshktruong@gmail.com>
2022-04-04 23:42:38 -0600

    Test demo of Calculator

--- src/pages/InteractiveCalculator/InteractiveCalculator.js
+++ src/pages/InteractiveCalculator/InteractiveCalculator.js
@@ -1,20 +1,14 @@
-import React, {useState} from 'react'
-import { Var, Scope } from "../../components/Calculator"
+import React from 'react'
+import { Var, Scope, Result, MathTex } from "../../components/Calculator"
 
-import  { useSelector, useDispatch } from 'react-redux';
-import { ADD_SCOPE, UPDATE_VARIABLE } from '../../constants'
 
-const InteractiveCalculator = () => {
-  const [val, setVal] = useState({});
-
-  const calculator = useSelector(state => state.calcReducer)
-  const dispatch = useDispatch();
-  
+const InteractiveCalculator = () => {  
   return (
     <div>
       <Scope scope='eq1' expr='k*n+1' />
-      <Var scope='eq1' var='k' min={1} max={10} />
-      <Var scope='eq1' var='n' min={1} max={10} />
+      If we let k = <Var scope='eq1' var='k' min={1} max={10} /> and
+      n = <Var scope='eq1' var='n' min={1} max={10} /> for the expression
+      f = <MathTex scope='eq1'/>, then f = <Result scope='eq1' />.
     </div>
   )
 }
