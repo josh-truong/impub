98f4b274edcda326f7f9c412276d9a55058dbe98
Joshua Truong <joshktruong@gmail.com>
2022-04-04 23:39:27 -0600

    Mathjs -> Tex component

---
+++ src/components/Calculator/js/MathTex.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Latex from '../../Latex'
+
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+const MathTex = (props) => {
+    const latexExpr = useSelector(state => {
+        try {
+            return state.calcReducer[props.scope].latexExpr;
+        } catch (error) { return null }
+    });
+    return (
+        <Latex expr={`$${latexExpr}$`} />
+    )
+}
+
+MathTex.propTypes = {
+    scope: PropTypes.string.isRequired
+};
+
+export default MathTex
