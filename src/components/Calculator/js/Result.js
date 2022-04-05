7d297d72df9c91b6821774669e837293a4ab31bd
Joshua Truong <joshktruong@gmail.com>
2022-04-04 23:40:22 -0600

    Retrieve computed expression

---
+++ src/components/Calculator/js/Result.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+const Result = (props) => {
+    const result = useSelector(state => {
+        try {
+            return state.calcReducer[props.scope].result;
+        } catch(error) { return null }
+    });
+
+    return (
+        <span style={{color:'#0000FF'}}>{result}</span>
+    )
+}
+
+Result.propTypes = {
+    scope: PropTypes.string.isRequired
+};
+
+export default Result
