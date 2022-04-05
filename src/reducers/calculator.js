3903c7d8130fff1e1814091117206ab4c8975535
Joshua Truong <joshktruong@gmail.com>
2022-04-04 23:41:39 -0600

    Helper functions

--- src/reducers/calculator.js
+++ src/reducers/calculator.js
@@ -1,33 +1,6 @@
 import { ADD_SCOPE, EVAL_EXPR, UPDATE_VARIABLE } from "../constants"
+import getVariables from "./helpers/getVariables";
 var math = require('mathjs')
-
-// function getVariables(dict, variables = {}) { // tail recursion method
-//     for (const [key, value] of Object.entries(dict)) {
-//         if (key === 'args' && dict[key] instanceof Array) {
-//             dict[key].forEach(elt => {
-//                 if (elt.hasOwnProperty('name')) {
-//                     variables[Object.values(elt)[0]] = null
-//                 }
-//                 else if (elt.hasOwnProperty('content')) {
-//                     getVariables(elt.content, variables)
-//                 } else {
-//                     getVariables(elt, variables)
-//                 }
-//             })
-//         }
-//     }
-//     return variables
-// }
-
-function getVariables(node) {
-    const filtered = node.filter(function (node) {
-        return (node.isSymbolNode && node.name)
-    })
-    const distinct = Array.from(new Set(filtered.map((elt) => elt.name)))
-    var var_dict = {}
-    distinct.map((elt) =>  var_dict[elt] = null)
-    return var_dict
-}
 
 const calcReducer = (state={}, action) => {
     switch (action.type) {
