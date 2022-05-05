c66e18ca83a7576dae334ab35a0407fb52db6f0a
Joshua Truong <joshktruong@gmail.com>
2022-05-05 01:26:40 -0600

    Updates

---
+++ precache-manifest.47f244bb25a519bae5dec1885cfd7df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "195ee8b6c61ab0820ab9",
+    "url": "/impub/static/css/main.778fa612.chunk.css"
+  },
+  {
+    "revision": "195ee8b6c61ab0820ab9",
+    "url": "/impub/static/js/main.195ee8b6.chunk.js"
+  },
+  {
+    "revision": "83915027d7687e7f20ff",
+    "url": "/impub/static/js/1.83915027.chunk.js"
+  },
+  {
+    "revision": "8e332a64e3dad19d4d2a",
+    "url": "/impub/static/js/runtime~main.8e332a64.js"
+  },
+  {
+    "revision": "cd8beb2e13bfdd3324fb895feabfe420",
+    "url": "/impub/static/media/fig1.cd8beb2e.png"
+  },
+  {
+    "revision": "4dc5a65f2fc31219e3d3b1fbc9136d88",
+    "url": "/impub/static/media/fig2.4dc5a65f.png"
+  },
+  {
+    "revision": "41bc1ad045eca7f297d875f7a3098356",
+    "url": "/impub/index.html"
+  }
+];
