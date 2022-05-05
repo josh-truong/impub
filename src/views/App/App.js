c53487902264b8b95504e4981ce24a8a305b5faa
Joshua Truong <joshktruong@gmail.com>
2022-05-05 11:11:28 -0600

    Spruce up

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -5,12 +5,17 @@ function App() {
   const tab = <span style={{ padding: "0 15px" }} />
   return (
     <div className="container">
-      <h5 style={{marginBottom:'60px'}}>
-        Contributors
-        <h6>
-        {tab}Joshua Grochow {tab}Joshua Truong
-        </h6>
-      </h5>
+      <div className='row' style={{ marginBottom: '60px' }}>
+        <div className='col'>
+          <h5>
+            Contributors
+          </h5>
+          {tab}Joshua Grochow {tab}Joshua Truong
+        </div>
+        <div className='col' style={{ textAlign: 'right' }}>
+          <a href='https://github.com/josh-truong/impub'>Github Source</a>
+        </div>
+      </div>
       <Demo />
     </div>
   );
