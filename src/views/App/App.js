a8b74b9a1d67a096b8abde65a0ae8fec4a4f0e31
Joshua Truong <joshktruong@gmail.com>
2022-05-05 01:50:10 -0600

    added contributors

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { Demo } from '../index.js';
 
 function App() {
+  const tab = <span style={{ padding: "0 15px" }} />
   return (
     <div className="container">
+      <h5 style={{marginBottom:'20px'}}>
+        Contributors
+        <h6>
+        {tab}Joshua Grochow {tab}Joshua Truong
+        </h6>
+      </h5>
       <Demo />
     </div>
   );
